5ec27683f862d542218e1bfa64b55ba568f1464d
Ro4052 <Ro4052@users.noreply.github.com>
2023-01-03 08:44:34 +0000

    add page title and favicon

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 
 export interface PageProps {
@@ -7,14 +8,19 @@ export interface PageProps {
 }
 
 export const Page = ({ children, showFooter = true }: PageProps) => (
-  <div className='flex flex-col h-screen p-4'>
-    <div className='grow'>
-      {children}
-    </div>
-    {showFooter && (
-      <div className='text-right text-xs'>
-        <Link className='hover:underline' href='/privacy-and-cookies'>Privacy and Cookies</Link>
+  <>
+    <Head>
+      <title>Runreport</title>
+    </Head>
+    <div className='flex flex-col h-screen p-4'>
+      <div className='grow'>
+        {children}
       </div>
-    )}
-  </div>
+      {showFooter && (
+        <div className='text-right text-xs'>
+          <Link className='hover:underline' href='/privacy-and-cookies'>Privacy and Cookies</Link>
+        </div>
+      )}
+    </div>
+  </>
 );
